05bd7b445e00e177270af389af4f25331e70a664
Zach Cochran <zcochran@ucsd.edu>
2025-04-17 14:32:46 -0700

    github change

--- global.js
+++ global.js
@@ -21,7 +21,7 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                 // Local
-  : "/zcochran27/portfolio/"; 
+  : "/portfolio/"; 
 
 for (let p of pages) {
     let url = p.url;
