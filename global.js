f1377968ebb921c6b88363d0fdd8bd289b06bdd1
Zach Cochran <zcochran@ucsd.edu>
2025-04-17 14:44:13 -0700

    removed testing

--- global.js
+++ global.js
@@ -3,12 +3,6 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-// const navLinks = $$("nav a");
-
-// const currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname
-// );
-// currentLink?.classList.add('current');
 
 let pages = [
     { url: 'index.html', title: 'Home' },
