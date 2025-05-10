3fca1776e37a7a3ccd00534b12c2a071acd0e04e
Zach Cochran <zcochran@ucsd.edu>
2025-05-10 12:08:17 -0700

    added meta to navbar

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'resume/', title: 'Resume'},
+    { url: 'meta/', title:'Meta'},
     { url: 'https://github.com/zcochran27', title: 'GitHub' }];
 let nav = document.createElement('nav');
 document.body.prepend(nav);
